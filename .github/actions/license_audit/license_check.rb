c26d8a69ff65ff1b3009165128cd97be655e272c
tim_clegg <10459168+timclegg@users.noreply.github.com>
2021-05-11 16:37:46 -0600

    More pipeline changes

---
+++ .github/actions/license_audit/license_check.rb
@@ -0,0 +1,45 @@
+require 'json'
+
+file_name = 'licenses.json'
+
+if ARGV.length > 0
+  file_name = ARGV[0]
+end
+
+if !File.exist?(file_name)
+  puts "ERROR - invalid (missing) file name given: #{file_name}"
+  exit!
+end
+
+file_data = ''
+open(file_name) do |f|
+  file_data += f.read
+end
+
+json_data = JSON.parse(file_data)
+
+unapproved_licenses = {}
+
+json_data['files'].each do |f|
+  if f['license_policy'].count <= 0
+    f['licenses'].each do |l|
+      if !unapproved_licenses.include?(l['key'])
+        unapproved_licenses[l['key']] = []
+      end
+      
+      if !unapproved_licenses[l['key']].include?(f['path'])
+        unapproved_licenses[l['key']] << f['path']
+      end
+    end
+  end
+end
+
+if unapproved_licenses.count > 0
+  puts "ERROR - found some licenses that require further inspection:\n#{unapproved_licenses}"
+  puts "::set-output name=unapproved_licenses::true"
+  exit!
+else
+  puts "All licenses found were approved for use."
+  puts "::set-output name=unapproved_licenses::false"
+  exit
+end
