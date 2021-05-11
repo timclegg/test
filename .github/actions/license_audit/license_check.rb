1bd7c6cbbba94f244c6ce3743f03cf39d981f066
tim_clegg <10459168+timclegg@users.noreply.github.com>
2021-05-11 17:20:11 -0600

    Updating pipeline

--- .github/actions/license_audit/license_check.rb
+++ .github/actions/license_audit/license_check.rb
@@ -2,48 +2,51 @@ require 'json'
 
 file_name = 'licenses.json'
 
-if ARGV.length > 0
-  file_name = ARGV[0]
-end
+puts "Hello world!"
+puts "ENV: #{ENV['INPUT_LICENSES_FILE']}"
+puts "ARGV: #{ARGV[0]}"
+# if ARGV.length > 0
+#   file_name = ARGV[0]
+# end
 
-if ENV.include?('INPUT_LICENSES_FILE') && ENV['INPUT_LICENSES_FILE'].to_s.length > 0
-  file_name = ENV['INPUT_LICENSES_FILE']
-end
+# if ENV.include?('INPUT_LICENSES_FILE') && ENV['INPUT_LICENSES_FILE'].to_s.length > 0
+#   file_name = ENV['INPUT_LICENSES_FILE']
+# end
 
-if !File.exist?(file_name)
-  puts "ERROR - invalid (missing) file name given: #{file_name}"
-  exit!
-end
+# if !File.exist?(file_name)
+#   puts "ERROR - invalid (missing) file name given: #{file_name}"
+#   exit!
+# end
 
-file_data = ''
-open(file_name) do |f|
-  file_data += f.read
-end
+# file_data = ''
+# open(file_name) do |f|
+#   file_data += f.read
+# end
 
-json_data = JSON.parse(file_data)
+# json_data = JSON.parse(file_data)
 
-unapproved_licenses = {}
+# unapproved_licenses = {}
 
-json_data['files'].each do |f|
-  if f['license_policy'].count <= 0
-    f['licenses'].each do |l|
-      if !unapproved_licenses.include?(l['key'])
-        unapproved_licenses[l['key']] = []
-      end
+# json_data['files'].each do |f|
+#   if f['license_policy'].count <= 0
+#     f['licenses'].each do |l|
+#       if !unapproved_licenses.include?(l['key'])
+#         unapproved_licenses[l['key']] = []
+#       end
       
-      if !unapproved_licenses[l['key']].include?(f['path'])
-        unapproved_licenses[l['key']] << f['path']
-      end
-    end
-  end
-end
+#       if !unapproved_licenses[l['key']].include?(f['path'])
+#         unapproved_licenses[l['key']] << f['path']
+#       end
+#     end
+#   end
+# end
 
-if unapproved_licenses.count > 0
-  puts "ERROR - found some licenses that require further inspection:\n#{unapproved_licenses}"
-  puts "::set-output name=unapproved_licenses::true"
-  exit!
-else
-  puts "All licenses found were approved for use."
-  puts "::set-output name=unapproved_licenses::false"
-  exit
-end
+# if unapproved_licenses.count > 0
+#   puts "ERROR - found some licenses that require further inspection:\n#{unapproved_licenses}"
+#   puts "::set-output name=unapproved_licenses::true"
+#   exit!
+# else
+#   puts "All licenses found were approved for use."
+#   puts "::set-output name=unapproved_licenses::false"
+#   exit
+# end
