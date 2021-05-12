2747c09c699e1f3658fb3c93c4c278af51f9470d
tim_clegg <10459168+timclegg@users.noreply.github.com>
2021-05-11 19:40:00 -0600

    Updating pipeline

--- .github/actions/license_audit/license_check.rb
+++ .github/actions/license_audit/license_check.rb
@@ -44,14 +44,14 @@ json_data['files'].each do |f|
   end
 end
 
-puts "Final output:"
+# puts "Final output:"
 
 if unapproved_licenses.count > 0
   puts "ERROR - found some licenses that require further inspection:\n#{unapproved_licenses}"
   puts "::set-output name=unapproved_licenses::true"
-  exit(1)
+  # exit(1)
 else
   puts "All licenses found were approved for use."
   puts "::set-output name=unapproved_licenses::false"
-  exit(0)
+  # exit(0)
 end
