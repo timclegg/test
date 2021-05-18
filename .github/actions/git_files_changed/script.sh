a2b579523169b0ef3b68afe98e76e7d7bbec5f87
tim_clegg <10459168+timclegg@users.noreply.github.com>
2021-05-18 09:44:14 -0600

    Updating pipeline

--- .github/actions/git_files_changed/script.sh
+++ .github/actions/git_files_changed/script.sh
@@ -3,6 +3,8 @@
 # Copyright (c) 2021 Oracle and/or its affiliates.
 #
 
+echo `ls -lah /github/workspace`
+
 echo "Looking back at the previous ${INPUT_NUMBER_COMMITS_BACK} commits"
 echo "::set-output name=all_files_changed::`git diff --name-only HEAD~$INPUT_NUMBER_COMMITS_BACK..HEAD`"
 echo "::set-output name=added_files::`git diff --name-only --diff-filter=A HEAD~$INPUT_NUMBER_COMMITS_BACK..HEAD`"
